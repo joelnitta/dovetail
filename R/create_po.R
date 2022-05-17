5adb8ac74810b86d1113bd51cea01bc2d457ba92
Joel Nitta <joelnitta@gmail.com>
2022-05-17 17:07:19 +0900

    Add example for create_po
    don't make po folder as side effect

--- R/create_po.R
+++ R/create_po.R
@@ -1,21 +1,37 @@
 #' Create a single PO file for translating a (R)markdown file
 #'
+#' Any folders that don't yet exist for the output path specified will be
+#' automatically created.
+#'
 #' @inheritParams md2po
+#' @param include_codeblocks Logical vector of length 1; should code blocks
+#' be included in the PO file? Default `TRUE`.
 #'
 #' @return List with components formatted like the output of [processx::run()].
 #' Externally, the PO file will be generated at the path specified by `po`.
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' # This example assumes the command is being run from the root of a
-#' # carpentries lesson.
-#' create_po("episodes/01-introduction.Rmd", "po/ja/01-introduction.po")
-#' }
-create_po <- function(md_in, po, wrap_width = 0, other_args = NULL) {
+#' # Write a PO file based on the example MD file in this package
+#' # to a temporary file, check the first few lines, then delete it
+#' temp_po <- tempfile(fileext = "po")
+#' create_po(
+#'   md_in = system.file("extdata", "test.Rmd", package = "dovetail"),
+#'   po = temp_po,
+#'   other_args = "--nolocation"
+#' )
+#' head(readLines(temp_po))
+#' unlink(temp_po)
+#'
+create_po <- function(
+    md_in, po, wrap_width = 0,
+    include_codeblocks = TRUE,
+    other_args = NULL) {
 
-  # Create po dir
-  fs::dir_create("po")
+  # Get path to output file
+  po_path <- fs::path_dir(po)
+  # Create output directory structure if it doesn't yet exist
+  fs::dir_create(po_path)
 
   # Pre-process MD for creating PO
   temp_md <- tempfile()
@@ -24,6 +40,11 @@ create_po <- function(md_in, po, wrap_width = 0, other_args = NULL) {
   md_lines <- readr::read_lines(md_in)
   md_lines <- exclude_fences(md_lines)
   readr::write_lines(md_lines, temp_md)
+
+  # Format arguments
+  codeblocks_arg <- NULL
+  if (isTRUE(include_codeblocks)) codeblocks_arg <- "-c"
+  other_args <- c(codeblocks_arg, other_args)
 
   # Make PO
   md2po(
