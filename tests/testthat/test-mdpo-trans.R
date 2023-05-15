59db08c603355be77a8b1155d7d81b27f32e9aee
Joel Nitta <joelnitta@gmail.com>
2023-05-15 10:53:33 +0900

    Allow selection of container fixes #10

--- tests/testthat/test-mdpo-trans.R
+++ tests/testthat/test-mdpo-trans.R
@@ -1,11 +1,20 @@
 temp_md_trans <- fs::path(tempdir(), "test.ja.md")
 
+po4a_container <- ifelse(
+  grepl("arm64", Sys.info()[["machine"]]),
+  "joelnitta/po4a-arm64:latest",
+  "joelnitta/po4a:latest"
+  )
+
 test_that("Translation works", {
+  # Run arm64 version of po4a on arm64 machines,
+  # default otherwise
   expect_snapshot_file(
     po2md(
       md_in = system.file("extdata", "test.Rmd", package = "dovetail"),
       po = system.file("extdata", "test.ja.po", package = "dovetail"),
-      md_out = temp_md_trans
+      md_out = temp_md_trans,
+      container_id = po4a_container
     ),
     "test.ja.md"
   )
